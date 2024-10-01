d51683da5747a596c1bdb4dd15f0bf6d01d53b41
Muhammad Fahad <muhammadfahadbinabid@gmail.com>
2024-10-01 22:05:17 +0500

    more progress on the app

--- src/app/components/main/account/login/login.component.ts
+++ src/app/components/main/account/login/login.component.ts
@@ -1,18 +1,32 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { User } from '../../../../models/user';
+
 import { AccountService } from '../../../../services/account.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Login } from '../../../../models/account/login';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  data?: User;
+  data: Login = { userName: '', password: '' };
 
   private service = inject(AccountService);
+
+  login(form: NgForm) {
+    console.log('form Data', form);
+    this.service.login(this.data).subscribe({
+      next: (response) => {
+        console.log('response is:', response);
+      },
+      error: (error) => {
+        console.log('error is:', error);
+      },
+    });
+  }
 }
