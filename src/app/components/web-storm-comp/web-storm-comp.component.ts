0874f4eff28e2d08d8b9f7c35e8f3b97a66b7a24
M-Fahad-Abid <muhammadfahadbinabid@gmail.com>
2024-12-07 20:39:42 +0500

    directly from webstorm

---
+++ src/app/components/web-storm-comp/web-storm-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-storm-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './web-storm-comp.component.html',
+  styleUrl: './web-storm-comp.component.css'
+})
+export class WebStormCompComponent {
+
+}
