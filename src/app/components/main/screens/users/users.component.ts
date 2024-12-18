21d3de3c97a3d69e246fe880ce7302900335c6b9
M-Fahad-Abid <muhammadfahadbinabid@gmail.com>
2024-12-18 18:29:32 +0500

    section 13 done

--- src/app/components/main/screens/users/users.component.ts
+++ src/app/components/main/screens/users/users.component.ts
@@ -1,32 +1,43 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../../../_services/members.service';
 import { CardComponent } from '../../../other/card/card.component';
-import { PageChangedEvent, PaginationModule } from 'ngx-bootstrap/pagination';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { AccountService } from '../../../../_services/account.service';
+import { UserParams } from '../../../../models/user-params';
+import { FormsModule } from '@angular/forms';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CardComponent, PaginationModule],
+  imports: [CardComponent, PaginationModule, FormsModule, ButtonsModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css',
 })
 export class UsersComponent implements OnInit {
-  public memberService = inject(MembersService);
-  // data: Member[] = [];
-  pageNumber = 1;
-  pageSize = 5;
+  memberService = inject(MembersService);
+
+  genderList = [
+    { value: 'male', display: 'Males' },
+    { value: 'female', display: 'Females' },
+  ];
 
   ngOnInit(): void {
     if (!this.memberService.paginatedResult()) this.getUsers();
   }
 
   getUsers() {
-    this.memberService.getAllUsers(this.pageNumber, this.pageSize);
+    this.memberService.getAllUsers();
+  }
+
+  resetFilters() {
+    this.memberService.resetUserParams();
+    this.getUsers();
   }
 
   pageChanged(event: any) {
-    if (this.pageNumber !== event.page) {
-      this.pageNumber = event.page;
+    if (this.memberService.userParams().pageNumber !== event.page) {
+      this.memberService.userParams().pageNumber = event.page;
       this.getUsers();
     }
   }
--- src/app/models/user-params.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UserParams } from './user-params';
-
-describe('UserParams', () => {
-  it('should create an instance', () => {
-    expect(new UserParams()).toBeTruthy();
-  });
-});
