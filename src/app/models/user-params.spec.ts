c04a7e999e3d5f03559bb37762b46c3c378d8a86
M-Fahad-Abid <muhammadfahadbinabid@gmail.com>
2024-12-18 18:28:31 +0500

    section-13 complete

---
+++ src/app/models/user-params.spec.ts
@@ -0,0 +1,7 @@
+import { UserParams } from './user-params';
+
+describe('UserParams', () => {
+  it('should create an instance', () => {
+    expect(new UserParams()).toBeTruthy();
+  });
+});
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,5 +1,6 @@
 export interface User {
   userName: string;
   token: string;
+  gender: string;
   photoUrl?: string;
 }
